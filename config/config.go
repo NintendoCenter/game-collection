ea39d725beb728db9ff0cd82e9eb1710d3f92548
Elijah Zobenko <ilya@zobenko.ru>
2021-08-20 01:44:57 +0300

    Disable Dockerfile

--- config/config.go
+++ config/config.go
@@ -16,5 +16,3 @@ func NewConfig() (*Config, error) {
 	cfg := new(Config)
 	return cfg, env.Parse(cfg)
 }
-
-//mongodb+srv://nc:qD96e254ngrFm837@private-nc-mongo-ce37f85a.mongo.ondigitalocean.com/admin?authSource=admin
